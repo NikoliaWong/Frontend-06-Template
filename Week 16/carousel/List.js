8583f43c8e9c2fc21f25cdfd4eea98c436d70739
NikoliaWong <447241178@qq.com>
2021-02-22 10:50:23 +0800

    week16

---
+++ Week 16/carousel/List.js
@@ -0,0 +1,17 @@
+import { Component, ATTRIBUTE } from './jsx';
+
+class Button extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    this.children = this[ATTRIBUTE].data.map(this.template);
+    this.root = (<div>{this.children}</div>).render();
+    return this.root;
+  }
+  appendChild(child) {
+    this.template = child;
+  }
+}
+
+export default Button;
---
+++ Week 16/gesture/index.js
@@ -0,0 +1,9 @@
+import Listener from './listener.js';
+import Recognizer from './recognizer.js';
+import Dispatcher from './dispatcher.js';
+
+// 元素监听手势
+const enableGesture = (ele) => {
+  new Listener(ele, new Recognizer(new Dispatcher(ele)));
+};
+export default enableGesture;
